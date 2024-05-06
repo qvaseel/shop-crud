54398f4066140d8156281aa69c7c45bdf550d194
qvaseel <118725399+qvaseel@users.noreply.github.com>
2024-05-07 02:15:22 +0500

    Update AddProductButton.tsx

--- src/components/ui/AddProductButton/AddProductButton.tsx
+++ src/components/ui/AddProductButton/AddProductButton.tsx
@@ -5,7 +5,7 @@ const AddProductButton = () => {
   return (
     <Link
       href={ROUTER.PRODUCT}
-      className=" bg-[#374A3D] rounded text-white text-2xl py-2 px-4 max-[630px]:text-sm"
+      className="hover:scale-105 transition-all duration-300 bg-[#374A3D] rounded text-white text-2xl py-2 px-4 max-[630px]:text-sm"
     >
       + Добавить товар
     </Link>
