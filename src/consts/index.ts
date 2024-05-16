873cfe5db79dda720d8b92918b7bd802e8b894aa
qvaseel <118725399+qvaseel@users.noreply.github.com>
2024-05-16 23:04:23 +0500

    Update index.ts

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,5 +1,5 @@
-export const api_url = "https://shop-server.up.railway.app"
-
+//export const api_url = "https://shop-server.up.railway.app"
+export const api_url = "https://shop-server-orpin.vercel.app"
 export const no_image =
   `${api_url}/src/uploads/no-image-large.jpg`;
 
