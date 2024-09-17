294c8a1d13ca6820306000d7f7ec185b534b02cd
qvaseel <118725399+qvaseel@users.noreply.github.com>
2024-09-17 19:17:16 +0500

    Update index.ts

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,5 +1,5 @@
-//export const api_url = "https://shop-server.up.railway.app"
-export const api_url = "https://shop-server-orpin.vercel.app"
-export const no_image = "https://cdn-icons-png.flaticon.com/512/4054/4054617.png"
-  //`${api_url}/src/uploads/no-image-large.jpg`;
+export const api_url = "https://shop-server.up.railway.app"
+// export const api_url = "https://shop-server-orpin.vercel.app"
+// export const no_image = "https://cdn-icons-png.flaticon.com/512/4054/4054617.png"
+  `${api_url}/src/uploads/no-image-large.jpg`;
 
